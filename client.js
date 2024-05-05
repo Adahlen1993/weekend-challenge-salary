7f7aae22df5cc1ac548673bbed5c77e99a850cb8
Alex <adahl1993@gmail.com>
2024-05-05 13:09:10 -0500

    finalized and reformatted

--- client.js
+++ client.js
@@ -1,7 +1,6 @@
-
-function onReady(){
-    alert(`Attention Employees! Please Fill Out The Form 
-    Below!`)
+function onReady() {
+  alert(`Attention Employees! Please Fill Out The Form 
+    Below!`);
 }
 // onReady();
 
@@ -15,26 +14,26 @@ let monthlySalary = 0;
 // 1.Wire up inputs
 // 2.Wire up form for submit
 function employeeForm(event) {
-    event.preventDefault();
-    event.target;
-    
-    firstName = document.getElementById('first-name');
-    lastName = document.getElementById('last-name');
-    idNumber = document.getElementById('id-number');
-    jobTitle = document.getElementById('job-title');
-    annualSalary = document.getElementById('annual-salary');
-    // 6. add employees to table
-    // document.getElementById('added-employee').innerHTML +=`
-    // <tr>
-    // <td>${firstName.value}</td>
-    // <td>${lastName.value}</td>
-    // <td>${idNumber.value}</td>
-    // <td>${jobTitle.value}</td>
-    // <td>${annualSalary.value}</td>
-    // <td><button id='remove-row' onclick='removeRow(event)'>Delete</button>
-    // </tr>
-    // `;
-    document.getElementById('added-employee').innerHTML +=`
+  event.preventDefault();
+  event.target;
+
+  firstName = document.getElementById("first-name");
+  lastName = document.getElementById("last-name");
+  idNumber = document.getElementById("id-number");
+  jobTitle = document.getElementById("job-title");
+  annualSalary = document.getElementById("annual-salary");
+  // 6. add employees to table
+  // document.getElementById('added-employee').innerHTML +=`
+  // <tr>
+  // <td>${firstName.value}</td>
+  // <td>${lastName.value}</td>
+  // <td>${idNumber.value}</td>
+  // <td>${jobTitle.value}</td>
+  // <td>${annualSalary.value}</td>
+  // <td><button id='remove-row' onclick='removeRow(event)'>Delete</button>
+  // </tr>
+  // `;
+  document.getElementById("added-employee").innerHTML += `
     <tr>
     <td class='emp'>${firstName.value}</td>
     <td class='emp'>${lastName.value}</td>
@@ -44,85 +43,91 @@ function employeeForm(event) {
     <td class='emp'><button id="remove-button" onclick='removeRow(event)'>Delete</button></td>
     </tr>
     `;
-    let employee = {
-        firstname: firstName.value,
-        lastname: lastName.value,
-        idnumber: idNumber.value,
-        jobtitle: jobTitle.value,
-        annualsalary: annualSalary.value
-    };
-    employees.push(employee);
-    // 4.Calculate Monthly costs
-    monthlySalary = annualSalary.value/12;
-    monthlyExpenses += monthlySalary;
-    // 5.add conditional formatting for monthly costs
-    //  if over 20000
-    if(monthlyExpenses === 20000){
-        document.getElementById('expenses').innerHTML = `
-        <h3 id="expenses" style="background-color:yellow;">$${monthlyExpenses.toFixed(2)}</h3>
-        `;}
-    else if(monthlyExpenses > 20000){
-            document.getElementById('expenses').innerHTML = `
-            <h3 id="expenses" style="background-color:red;">$${monthlyExpenses.toFixed(2)}</h3>
+  let employee = {
+    firstname: firstName.value,
+    lastname: lastName.value,
+    idnumber: idNumber.value,
+    jobtitle: jobTitle.value,
+    annualsalary: annualSalary.value,
+  };
+  employees.push(employee);
+  // 4.Calculate Monthly costs
+  monthlySalary = annualSalary.value / 12;
+  monthlyExpenses += monthlySalary;
+  // 5.add conditional formatting for monthly costs
+  //  if over 20000
+  if (monthlyExpenses === 20000) {
+    document.getElementById("expenses").innerHTML = `
+        <h3 id="expenses" style="background-color:yellow;">$${monthlyExpenses.toFixed(
+          2
+        )}</h3>
+        `;
+  } else if (monthlyExpenses > 20000) {
+    document.getElementById("expenses").innerHTML = `
+            <h3 id="expenses" style="background-color:red;">$${monthlyExpenses.toFixed(
+              2
+            )}</h3>
             `;
-    } else {document.getElementById('expenses').innerHTML = `
+  } else {
+    document.getElementById("expenses").innerHTML = `
     <h3 id="expenses" >$${monthlyExpenses.toFixed(2)}</h3>
-    `}
-    // 3.Clear form after submit
-    document.getElementById('employee-form').reset();
+    `;
+  }
+  // 3.Clear form after submit
+  document.getElementById("employee-form").reset();
 }
 
 // 6. create a delete button that clears whole row
 function removeRow(event) {
-    let salaryAttempt = document.getElementById('salary').innerHTML;
-    Number(salaryAttempt);
-   totalExpenses += salaryAttempt/12;
-   monthlyExpenses -= totalExpenses;
-   if(monthlyExpenses === 20000){
-    document.getElementById('expenses').innerHTML = `
-    <h3 id="expenses" style="background-color:yellow;">$${monthlyExpenses.toFixed(2)}</h3>
-    `;}
-else if(monthlyExpenses > 20000){
-        document.getElementById('expenses').innerHTML = `
-        <h3 id="expenses" style="background-color:red;">$${monthlyExpenses.toFixed(2)}</h3>
+  let salaryAttempt = document.getElementById("salary").innerHTML;
+  Number(salaryAttempt);
+  totalExpenses += salaryAttempt / 12;
+  monthlyExpenses -= totalExpenses;
+  if (monthlyExpenses === 20000) {
+    document.getElementById("expenses").innerHTML = `
+    <h3 id="expenses" style="background-color:yellow;">$${monthlyExpenses.toFixed(
+      2
+    )}</h3>
+    `;
+  } else if (monthlyExpenses > 20000) {
+    document.getElementById("expenses").innerHTML = `
+        <h3 id="expenses" style="background-color:red;">$${monthlyExpenses.toFixed(
+          2
+        )}</h3>
         `;
-} else {document.getElementById('expenses').innerHTML = `
+  } else {
+    document.getElementById("expenses").innerHTML = `
 <h3 id="expenses" >$${monthlyExpenses.toFixed(2)}</h3>
-`}
-totalExpenses = 0;
-   
+`;
+  }
+  totalExpenses = 0;
 
-    
+  event.target.closest("tr").remove();
+}
+// potential loop to update dom
+// couldn't get splice to work with it
+//  let empID = document.getElementById('empid').innerHTML;
+//  for (let i=0; i<employees.length; i++) {
+//          if(employees[i].idnumber === empID && employees[i].annualsalary === salaryAttempt) {
+//                 employees.splice(employees[i],1);
+//              }
+//          }
+//          for (let employee of employees) {
+//                  let annSal = 0;
+//                  annSal += employee.annualsalary/12;
+//                  monthlyExpenses = annSal;
+//                  document.getElementById('expenses').innerHTML = monthlyExpenses;
 
-    event.target.closest('tr').remove();
-     
-     
-    }
-    
-    
-    
-    //  let empID = document.getElementById('empid').innerHTML;
-    //  for (let i=0; i<employees.length; i++) {
-    //          if(employees[i].idnumber === empID && employees[i].annualsalary === salaryAttempt) {
-    //                 employees.splice(employees[i],1);
-    //              }
-    //          }
-    //          for (let employee of employees) {
-    //                  let annSal = 0;
-    //                  annSal += employee.annualsalary/12;
-    //                  monthlyExpenses = annSal;
-    //                  document.getElementById('expenses').innerHTML = monthlyExpenses;
-             
-    //              }
-    // let employeeid = document.getElementById(
-        // for (employee of employees) {
-            //     if(employees.id === )
-            // }
-            // if(loopExpenses < 20000) {
-    //     document.getElementById('expenses').innerHTML = `
-    //     <h3 id="expenses" >${loopExpenses}</h3>
-    //     `;
-    // } else if(loopExpenses === 20000){
+//              }
+// let employeeid = document.getElementById(
+// for (employee of employees) {
+//     if(employees.id === )
+// }
+// if(loopExpenses < 20000) {
+//     document.getElementById('expenses').innerHTML = `
+//     <h3 id="expenses" >${loopExpenses}</h3>
+//     `;
+// } else if(loopExpenses === 20000){
 //     document.getElementById('expenses').innerHTML =`
 //     <h3 id="expenses" style="background-color:yellow;">${loopExpenses}</h3>
 //     `
